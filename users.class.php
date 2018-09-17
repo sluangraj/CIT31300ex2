0bc5b53b3c3eaf836f5deac877078132c13a6c43
sluangraj <42587174+sluangraj@users.noreply.github.com>
2018-09-16 22:43:24 -0400

    Add files via upload

---
+++ users.class.php
@@ -0,0 +1,27 @@
+<?
+class users {
+  protected $user_id;
+  protected $user_type;
+  protected $first_name;
+  protected $last_name;
+  protected $email_address;
+  protected $user_level;
+
+  public function __construct() {
+    $this->user_id = "sluangra";
+
+  }
+  public function __set($name,$value) {
+
+  }
+  public function __get($name){
+
+
+  }
+
+  public function __destruct(){
+
+  }
+
+}
+?>
